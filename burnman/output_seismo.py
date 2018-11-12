5d4ba05900184b99f64f467da37acab7cf177d19
sannecottaar <sanne.cottaar@gmail.com>
2018-11-12 16:37:16 +0000

    modify axisem output

--- burnman/output_seismo.py
+++ burnman/output_seismo.py
@@ -19,7 +19,7 @@ from .planet import Planet
 from .layer import Layer
 
 
-def write_tvel_file(planet_or_layer, filename='burnmanmodel.tvel', background_model=None):
+def write_tvel_file(planet_or_layer, modelname='burnmanmodel', background_model=None):
     """
     Function to write input file for obspy travel time calculations.
     Note: Because density isn't defined for most 1D seismic models, densities are output as zeroes.  The tvel format has a column for density, but this column is not used by obspy for travel time calculations.
@@ -41,7 +41,7 @@ def write_tvel_file(planet_or_layer, filename='burnmanmodel.tvel', background_mo
         layer = planet_or_layer
         depths = background_model.internal_depth_list()
         above_layer = np.where(depths < (np.max(depths) - layer.outer_radius))[-1]
-        below_layer = np.where( depths > (np.max(depths) -layer.inner_radius))[0]
+        below_layer = np.where(depths > (np.max(depths) -layer.inner_radius))[0]
         
         data_above = list(zip(depths[above_layer] / 1.e3,
                               background_model.v_p(depths[above_layer]) / 1.e3,
@@ -57,8 +57,8 @@ def write_tvel_file(planet_or_layer, filename='burnmanmodel.tvel', background_mo
                               np.zeros_like(depths[below_layer])))
 
         data = data_above + data_layer + data_below
-
-        with open(filename, 'wb') as f:
+        
+        with open(modelname+'.tvel', 'wb') as f:
             np.savetxt(f, data, header = layer.name + ' model from BurnMan between  a radius of ' +
                             str(layer.inner_radius) + ' and ' + str(layer.outer_radius) +' km \n'+
                        background_model.__class__.__name__ +
@@ -72,44 +72,32 @@ def write_tvel_file(planet_or_layer, filename='burnmanmodel.tvel', background_mo
                          planet.v_s[::-1] /1.e3,
                          planet.density[::- 1] / 1.e3))
 
-        with open(filename, 'wb') as f:
+        with open(modelname+'.tvel', 'wb') as f:
             np.savetxt(f, data, header = planet.name + ' model from BurnMan with a radius of ' +
                        str(planet.radius_planet) + ' km \n Layers of planet are ' +
                        ", ".join(layer.name for layer in planet.layers), fmt='%5.2f', delimiter='\t')
 
 
 
-def write_axisem_input( rock,  min_depth=670.e3, max_depth=2890.e3, T0=1900,
-        filename='axisem_burnmantestrock.txt', axisem_ref='axisem_prem_ani_noocean.txt', plotting=False):
+def write_axisem_input(planet_or_layer, modelname='burnmanmodel', axisem_ref = 'axisem_prem_ani_noocean.txt' , plotting = False):
     """
     Writing velocities and densities to AXISEM (www.axisem.info) input file
-    Default is set to replacing the lower mantle with the BurnMan rock
-    Note:
-        - This implementation uses PREM to convert from depths to pressures to compute at
-        - This implementation assumes an adiabatic temperature profile, only T0 at min_depth can be set
-        - Currently, it only honors the discontinuities already in the synthetic input file, so it is best
-            to only replace certain layers with burnman values (this should be improved in the future).
 
 
     Parameters
     ----------
-    rock  :  burnman.Composite()
-        Composition to implement in the model
-    min_depth : float
-        minimum depth to replace model (m) (default = 670 km)
-    max_depth : float
-        minimum depth to replace model (m) (default = 2890 km)
-    T0 : float
-        Anchor temperature at min_depth for adiabatic profile (K) (default=1900)
-    filename: string
-        Output filename (default ='axisem_burnmantestrock.txt')
-    axisem_ref: string
-        Input filename (in burnman/data/input_seismic/) (default = 'axisem_prem_ani_noocean.txt')
-    plotting: Boolean
-        True means plot of the old model and replaced model will be shown (default = False)
+    planet_or_layer  :  burnman.Planet() or burnman.Layer()
+    Planet or layer to write out to tvel file
+    modelname : string
+    Name of model, appears in name of output file
+    axisem_ref : string
+    Reference file, used to copy the header and for the rest of the planet, in the case of a Layer(), default ='axisem_prem_ani_noocean.txt'
 
     """
 
+
+    if not isinstance(planet_or_layer, (Planet, Layer)):
+        raise TypeError("Input must be a Planet() or Layer() ")
     # Load reference input
     datastream = pkgutil.get_data(
         'burnman', 'data/input_seismic/' + axisem_ref)
@@ -122,58 +110,55 @@ def write_axisem_input( rock,  min_depth=670.e3, max_depth=2890.e3, T0=1900,
             if line[0] != "#" and line[0] != "%":
                 table.append(numbers)
     table = np.array(table)
-    ref_radius = table[:, 0]
-    ref_depth = 6371.e3 - ref_radius
+    ref_radius  = table[:, 0]
+    ref_depth   = 6371.e3 - ref_radius
     ref_density = table[:, 1]
     ref_vpv = table[:, 2]
     ref_vsv = table[:, 3]
-    ref_Qk = table[:, 4]
+    ref_Qk  = table[:, 4]
     ref_Qmu = table[:, 5]
     ref_vph = table[:, 6]
     ref_vsh = table[:, 7]
     ref_eta = table[:, 8]
 
-    # Cutting out range to input in Axisem reference file (currently the lower
-    # mantle)
-    indrange = [x for x in range(len(ref_depth)) if ref_depth[
-        x] > min_depth and ref_depth[x] < max_depth]
-    # pad both ends to include up to discontinuity, bit of a hack...
-    indrange.insert(0, indrange[0] - 1)
-    indrange.append(indrange[-1] + 1)
-    # Invert depthrange so adiabatic computations work!
-    depthrange = ref_depth[indrange]
 
-    # convert depths to pressures
-    pressures = seismic.PREM().pressure(depthrange)
+    
+    if isinstance(planet_or_layer, Layer):
+        layer = planet_or_layer
+        # Cutting out range to input in Axisem reference file
 
-    # Computing adiabatic temperatures. T0 is an input parameter!
-    T0 = T0  # K
-    temperatures = geotherm.adiabatic(pressures, T0, rock)
+        i_min = np.argmin(np.abs(ref_radius-layer.outer_radius))
+        i_max = np.argmin(np.abs(ref_radius-layer.inner_radius))
+        layer_vp, layer_vs, layer_density = layer.evaluate(['v_p', 'v_s', 'density'], radlist = ref_radius[i_min:i_max], radius_planet = np.max(ref_radius))
+    
 
-    print("Calculations are done for:")
-    rock.debug_print()
+        ref_vpv[i_min:i_max] = layer_vp
+        ref_vph[i_min:i_max] = layer_vp
+        ref_vsv[i_min:i_max] = layer_vs
+        ref_vsh[i_min:i_max] = layer_vs
+        ref_density[i_min:i_max] = layer_density
 
-    rock_vp, rock_vs, rock_rho = rock.evaluate(
-        ['v_p', 'v_s', 'density'], pressures, temperatures)
-    discontinuity = 0
     # WRITE OUT FILE
+    filename ='axisem_'+modelname + '.txt'
     f = open(filename, 'w')
     print('Writing ' + filename + ' ...')
     f.write('# Input file ' +
-            filename +
+            modelname +
             ' for AXISEM created using BurnMan, replacing ' +
             axisem_ref +
             ' between ' +
-            str(np.round(min_depth /
+            str(np.round(layer.inner_radius /
                          1.e3)) +
             ' and ' +
-            str(np.round(max_depth /
+            str(np.round(layer.outer_radius/
                          1.e3)) +
             ' km \n')
-    f.write('NAME ' + filename + '\n')
+
+    discontinuity = 0 # Number discontinuities
+    f.write('NAME ' + modelname + '\n')
     for line in lines[2:18]:
         f.write(line[:-1] + '\n')
-    for i in range(indrange[0]):
+    for i in range(len(ref_radius)):
         if i > 0 and ref_radius[i] == ref_radius[i - 1]:
             discontinuity = discontinuity + 1
             f.write('#          Discontinuity   ' +
@@ -187,77 +172,27 @@ def write_axisem_input( rock,  min_depth=670.e3, max_depth=2890.e3, T0=1900,
             (ref_radius[i], ref_density[i], ref_vpv[i], ref_vsv[i], ref_Qk[i],
                 ref_Qmu[i], ref_vph[i], ref_vsh[i], ref_eta[i]))
 
-    for i in range(indrange[0], indrange[-1]):
-        ind2 = -1 + i - indrange[0]
-        if ref_radius[i] == ref_radius[i - 1]:
-            discontinuity = discontinuity + 1
-            f.write('#          Discontinuity   ' +
-                    str(discontinuity) +
-                    ', depth:    ' +
-                    str(np.round(ref_depth[i] /
-                                 1.e3, decimals=2)) +
-                    ' km \n')
-        f.write(
-            '%8.0f %9.2f %9.2f %9.2f %9.1f %9.1f %9.2f %9.2f %9.5f \n' %
-            (ref_radius[i],
-             rock_rho[ind2],
-             rock_vp[ind2],
-             rock_vs[ind2],
-             ref_Qk[i],
-                ref_Qmu[i],
-                rock_vp[ind2],
-                rock_vs[ind2],
-                ref_eta[i]))
 
-    for i in range(indrange[-1], len(ref_radius)):
-        if ref_radius[i] == ref_radius[i - 1]:
-            discontinuity = discontinuity + 1
-            f.write('#          Discontinuity   ' +
-                    str(discontinuity) +
-                    ', depth:    ' +
-                    str(np.round(ref_depth[i] /
-                                 1.e3, decimals=2)) +
-                    ' km \n')
-        f.write(
-            '%8.0f %9.2f %9.2f %9.2f %9.1f %9.1f %9.2f %9.2f %9.5f \n' %
-            (ref_radius[i], ref_density[i], ref_vpv[i], ref_vsv[i], ref_Qk[i],
-             ref_Qmu[i], ref_vph[i], ref_vsh[i], ref_eta[i]))
 
     f.close()
 
     if plotting:
         # plot vp
-        plt.plot(
-            ref_depth / 1.e3,
-            ref_vph / 1.e3,
-            color='g',
-            linestyle='-',
-            label='vp')
-        plt.plot(depthrange / 1.e3, rock_vp / 1.e3, color='g', linestyle='-',
+
+        plt.plot(ref_radius / 1.e3, ref_vpv / 1.e3, color='g', linestyle='-',
                  marker='o', markerfacecolor='g', markersize=1)
 
         # plot Vs
-        plt.plot(
-            ref_depth / 1.e3,
-            ref_vsh / 1.e3,
-            color='b',
-            linestyle='-',
-            label='vs')
-        plt.plot(depthrange / 1.e3, rock_vs / 1.e3, color='b', linestyle='-',
+
+        plt.plot(ref_radius / 1.e3, ref_vsv / 1.e3, color='b', linestyle='-',
                  marker='o', markerfacecolor='b', markersize=1)
 
         # plot density
-        plt.plot(
-            ref_depth / 1.e3,
-            ref_density / 1.e3,
-            color='r',
-            linestyle='-',
-            label='density')
-        plt.plot(depthrange / 1.e3, rock_rho / 1.e3, color='r', linestyle='-',
+        plt.plot(ref_radius / 1.e3, ref_density / 1.e3, color='r', linestyle='-',
                  marker='o', markerfacecolor='r', markersize=1)
 
         plt.title(filename + ' = ' + axisem_ref + ' replaced  between ' +
-                  str(min_depth / 1.e3) + ' and ' + str(max_depth / 1.e3) + ' km')
+                  str(layer.inner_radius / 1.e3) + ' and ' + str(layer.outer_radius / 1.e3) + ' km')
         plt.legend(loc='lower right')
         plt.show()
 
